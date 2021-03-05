4f79e6d9d6f74e9650140b94ef14758e70890068
Kat Buchanan <katrbuch@gmail.com>
2021-03-05 10:09:42 -0600

    troubleshooting modularized files

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-const connection = require("./src/connection");
 const inquirer = require("inquirer");
+const mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    multipleStatements: true, 
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: '3025880k!',
+    database: 'employee_db'
+  });
+  
+  connection.connect(function(err) {
+    if (err) throw err;
+    console.log(`You're tapped in at connection ID ${connection.threadId}\n`);
+    start();
+  });
 
 function start() {
   inquirer.prompt({
