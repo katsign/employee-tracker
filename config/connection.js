0eec820d73e58561b4b3a3acd40334ab731b06b5
Kat Buchanan <katrbuch@gmail.com>
2021-03-05 11:14:04 -0600

    setting up new file structure

---
+++ config/connection.js
@@ -0,0 +1,13 @@
+const mysql = require('mysql2');
+
+require('dotenv').config();
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  password: process.env.MYSQL_PASSWORD,
+  database: 'employees'
+});
+
+module.exports = connection;
